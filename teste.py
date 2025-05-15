df1d137a28429dc7974488d31f4bbdb5bc03c423
tuê <hoffmann3701@gmail.com>
2025-05-14 21:51:22 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import base64, codecs
 magic = '\\x68\\x6f\\x6f\\x6b\\x20\\x3d\\x20\\x22'
-love_rot13 = codecs.encode("Oi, teste teste teste ...", 'rot_13')
+love_rot13 = codecs.encode("", 'rot_13')
 love_base64 = base64.b64encode(love_rot13.encode('utf-8')).decode('utf-8')
 love_hex = ''.join([f'\\x{ord(c):02x}' for c in love_base64])
 love = f"'{love_hex}'"
